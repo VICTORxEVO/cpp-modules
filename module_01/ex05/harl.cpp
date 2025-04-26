6a1727ea8c02ecb2f49788872f83d1772bc20a81
VICTORxEVO <yassirsbai04@gmail.com>
2025-04-26 21:12:01 +0100

    add ex05

---
+++ module_01/ex05/harl.cpp
@@ -0,0 +1,37 @@
+#include "harl.hpp"
+
+void Harl::debug( void )
+{
+    std::cout << "DEBUG: 7XL-double-cheese" << std::endl;
+}
+
+
+void Harl::warning( void )
+{
+    std::cout << "WARNING: extra bacon for free" << std::endl;
+}
+
+void Harl::info( void )
+{
+    std::cout << "INFO: adding extra bacon" << std::endl;
+}
+void Harl::error( void )
+{
+    std::cout << "ERROR: This is unacceptable! " << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+    std::string info[] = {"DEBUG", "WARNING", "INFO", "ERROR"};
+    void (Harl::*ptr[4])() = {&Harl::debug, &Harl::warning, &Harl::info, &Harl::error};
+
+    for(int i = 0; i < 4; i++)
+    {
+        if (info[i] == level)
+        {
+            (this->*ptr[i])();
+            return ;
+        }
+    }
+    std::cout << "Invalid level!" << std::endl;
+}
