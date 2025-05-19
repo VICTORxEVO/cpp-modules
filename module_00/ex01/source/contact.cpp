fcce814140276eda2067f57594c057b51720bd0b
yassir sbai <yassirsbai04@gmail.com>
2025-05-19 16:35:38 +0100

    refactor: rename and implement setter methods for contact information

--- module_00/ex01/source/contact.cpp
+++ module_00/ex01/source/contact.cpp
@@ -1,31 +1,51 @@
 #include "phonebook.hpp"
 
-void contact::WriteFirstName(std::string str)
+void contact::SetPhoneNum(std::string str)
+{
+    this->info[3] = str;
+}
+
+void contact::SetSecret(std::string str)
+{
+    this->info[5] = str;
+}
+
+void contact::SetFirstName(std::string str)
 {
     this->info[0] = str;
 }
 
-void contact::WriteLastName(std::string str)
+void contact::SetLastName(std::string str)
 {
     this->info[1] = str;
 }
 
-void contact::WriteNickName(std::string str)
+void contact::SetNickName(std::string str)
 {
     this->info[2] = str;
 }
 
 std::string contact::GetFirstName(void) const
 {
-    return this->info[0];
+    return (this->info[0]);
 }
 
 std::string contact::GetLastName(void) const
 {
-    return this->info[1];
+    return (this->info[1]);
 }
 
 std::string contact::GetNickName(void) const
 {
-    return this->info[2];
+    return (this->info[2]);
+}
+
+std::string contact::GetPhoneNum() const
+{
+    return (this->info[3]);
+}
+
+std::string contact::GetSecret() const
+{
+    return (this->info[5]);
 }
