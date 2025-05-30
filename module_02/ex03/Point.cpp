d166b52696696982c6f320f0c3b2653065e33c72
yassir sbai <yassirsbai04@gmail.com>
2025-05-30 16:04:52 +0100

    move: funtions to its own file

---
+++ module_02/ex03/Point.cpp
@@ -0,0 +1,40 @@
+#include "Point.hpp"
+
+Fixed area(const Point &a, const Point &b, const Point &c)
+{
+    Fixed area = (a.getX() * (b.getY() - c.getY()) +
+                 b.getX() * (c.getY() - a.getY()) +
+                 c.getX() * (a.getY() - b.getY())) / 2;
+
+    return (area < 0) ? area * -1 : area;
+}
+
+Point::Point(): x(0), y(0)
+{
+    return ;
+}
+
+Point::Point(const float x, const float y) : x(x), y(y)
+{
+    return ;
+}
+
+Point::Point(const Point &other) : x(other.x), y(other.y)
+{
+    return ;
+}
+
+Point::~Point()
+{
+    return ;
+}
+
+Fixed Point::getX() const
+{
+    return x;
+}
+
+Fixed Point::getY() const
+{
+    return y;
+}
---
+++ module_02/ex03/Point.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Fixed.hpp"
+
+class Point
+{
+    private:
+        const Fixed x;
+        const Fixed y;
+    public:
+        Point();
+        Point(const float x, const float y);
+        Point(const Point &other);
+        ~Point();
+
+        Fixed getX() const;
+        Fixed getY() const;
+        
+};
+
+bool bsp(const Point a, const Point b, const Point c, const Point point);
+Fixed area(const Point &a, const Point &b, const Point &c);
