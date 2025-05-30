187b10d5dd751abe0e16837d04046d3a0eb1d668
yassir sbai <yassirsbai04@gmail.com>
2025-05-30 16:05:26 +0100

    move: area() and bsp() to other file

--- module_02/ex03/main.cpp
+++ module_02/ex03/main.cpp
@@ -1,27 +1,7 @@
 #include "Fixed.hpp"
+#include "Point.hpp"
 
-Fixed area(const Point &a, const Point &b, const Point &c)
-{
-    Fixed area = (a.getX() * (b.getY() - c.getY()) +
-                 b.getX() * (c.getY() - a.getY()) +
-                 c.getX() * (a.getY() - b.getY())) / 2;
 
-    return (area < 0) ? area * -1 : area;
-}
-
-bool bsp(const Point &a, const Point &b, const Point &c, const Point &point)
-{
-    
-    Fixed total_area = area(a, b, c);
-    //calculate area of points PBC, APC, ABP
-    Fixed area1 = area(point, b, c);
-    Fixed area2 = area(a, point, c);
-    Fixed area3 = area(a, b, point);
-    
-    if (area1 > 0, area2 > 0, area3 > 0)
-        return ((area1 + area2 + area3) == total_area);
-    return (false);
-}
 int main()
 {
     Point a(1, 2);
