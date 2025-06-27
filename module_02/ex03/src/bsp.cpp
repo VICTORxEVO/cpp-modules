ebbdb6620da08ba687a51692fcc587c64deb9a47
VICTORxEVO <yassirsbai04@gmail.com>
2025-06-27 15:11:58 +0100

    fix: correct logical condition in bsp function for area checks

--- module_02/ex03/src/bsp.cpp
+++ module_02/ex03/src/bsp.cpp
@@ -10,7 +10,7 @@ bool bsp(const Point a, const Point b, const Point c, const Point point)
     Fixed area2 = area(a, point, c);
     Fixed area3 = area(a, b, point);
     
-    if (area1 > 0, area2 > 0, area3 > 0)
+    if (area1 > 0 || area2 > 0 || area3 > 0)
         return ((area1 + area2 + area3) == total_area);
     return (false);
 }
