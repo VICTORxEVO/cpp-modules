3aebc2f577c4f41f6f5fdc9b00119136b49e1a93
VICTORxEVO <yassirsbai04@gmail.com>
2025-06-27 10:52:50 +0100

    refactor: update point coordinates and input prompts in main function

--- module_02/ex03/src/main.cpp
+++ module_02/ex03/src/main.cpp
@@ -4,11 +4,17 @@
 
 int main()
 {
-    Point a(1, 2);
-    Point b(4, 6);
-    Point c(5, 2);
-    Point point(10, 7);
+    Point a(0, 0);
+    Point b(0, 5);
+    Point c(5, 0);
+    int px, py;
+    std::cout << "enter x location of the point" << std::endl;
+    std::cin >> px;
 
+    std::cout << "enter y location of the point" << std::endl;
+    std::cin >> py;
+
+    Point point(px, py);
     std::cout << "Point is " << (bsp(a, b, c, point) ? "inside" : "outside") 
               << " the triangle" << std::endl;
 }
