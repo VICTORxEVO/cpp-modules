b937e0066ebee49b3a3088962682b929739ddb29
ViktorEvo <yassirsbai04@gmail.com>
2025-12-09 12:11:19 +0100

    add more tests

--- module_05/ex01/main.cpp
+++ module_05/ex01/main.cpp
@@ -7,11 +7,14 @@ int	main(void)
 	{
 		Bureaucrat	a("Alice", 50);
 		Form		f("Contract", 40, 20);
+		Form		f2("Covenant", 100, 50);
 
 		std::cout << a << std::endl;
 		std::cout << f << std::endl;
+		std::cout << f2 << std::endl;
 
 		a.signForm(f); //  fail (50 > 40)
+		a.signForm(f2);// succeed (50 < 100)
 
 		Bureaucrat	b("Boss", 1);
 		b.signForm(f); //  succeed
